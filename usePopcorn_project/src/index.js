0c17172406e58be236cc8b50ede4414665edc66b
Hana Ashraf <hana2002.ashraf@gmail.com>
2024-12-26 14:46:32 +0200

    Adding project files

---
+++ usePopcorn_project/src/index.js
@@ -0,0 +1,29 @@
+import React /*, { useState }*/ from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
+import reportWebVitals from "./reportWebVitals";
+
+// function MovieRating() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating}</p>
+//     </div>
+//   );
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <MovieRating /> */}
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
